51bd516399da2729c752ad8db357ba014693da0b
bmartinko <bennett.martinko@gmail.com>
2018-11-01 21:02:26 -0400

    YES I AM DONE

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -284,7 +284,11 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"OOF",
+			"This is repetitive",
+			"Hopefully I get an A this marking period",
+			"Yay this is done"
 	};
 	
 }
